5d153f9ae695f6a09bdd51500fb506cb5aeffb85
vanphuoc <vanphuoc3012@gmail.com>
2023-08-08 17:32:00 +0700

    cm

---
+++ src/main/java/com/phuoc/learn/alg/sorting/MaxPQ.java
@@ -0,0 +1,60 @@
+package com.phuoc.learn.alg.sorting;
+
+public class MaxPQ<Key extends Comparable<Key>> {
+    private final Key[] pq;
+    private int N = 0;
+
+    public MaxPQ(int maxN) {
+        pq = (Key[]) new Comparable[maxN + 1];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void insert(Key v) {
+        pq[++N] = v;
+        swim(N);
+    }
+
+    public Key delMax() {
+        Key max = pq[1];
+        exchange(1, N);
+        pq[N] = null;
+        N--;
+        sink(1);
+        return max;
+    }
+
+    //helper methods:
+    private boolean less(int i, int j) {
+        return pq[i].compareTo(pq[j]) < 0;
+    }
+
+    private void exchange(int i, int j) {
+        Key t = pq[i];
+        pq[j] = pq[i];
+        pq[i] = t;
+    }
+
+    private void swim(int k) {
+        while (k > 1 && less(k / 2, k)) {
+            exchange(k / 2, k);
+            k = k / 2;
+        }
+    }
+
+    private void sink(int k) {
+        while (2 * k <= N) {
+            int j = 2 * k;
+            if (j < N && less(j, j + 1)) j++;
+            if (!less(k, j)) break;
+            exchange(k, j);
+            k = j;
+        }
+    }
+}
