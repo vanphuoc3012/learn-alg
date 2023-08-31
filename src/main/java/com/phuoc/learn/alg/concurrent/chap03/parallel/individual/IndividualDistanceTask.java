157fab33d47b7d5ccbd06d63a536b64757c97eb8
vanphuoc <vanphuoc3012@gmail.com>
2023-08-31 23:31:47 +0700

    knn parallel individual

---
+++ src/main/java/com/phuoc/learn/alg/concurrent/chap03/parallel/individual/IndividualDistanceTask.java
@@ -0,0 +1,37 @@
+package com.phuoc.learn.alg.concurrent.chap03.parallel.individual;
+
+import com.phuoc.learn.alg.concurrent.chap03.data.Distance;
+import com.phuoc.learn.alg.concurrent.chap03.data.Sample;
+import com.phuoc.learn.alg.concurrent.chap03.distances.EuclideanDistanceCalculator;
+
+import java.util.concurrent.CountDownLatch;
+
+public class IndividualDistanceTask implements Runnable {
+
+    private final Distance[] distances;
+    private final int index;
+    private final Sample localExample;
+    private final Sample example;
+    private final CountDownLatch endController;
+
+    public IndividualDistanceTask(Distance[] distances,
+                                  int index,
+                                  Sample localExample,
+                                  Sample example,
+                                  CountDownLatch endController) {
+        this.distances = distances;
+        this.index = index;
+        this.localExample = localExample;
+        this.example = example;
+        this.endController = endController;
+    }
+
+    @Override
+    public void run() {
+        Distance dist = new Distance();
+        dist.setIndex(index);
+        dist.setDistance(EuclideanDistanceCalculator.calculate(localExample, example));
+        distances[index] = dist;
+        endController.countDown();
+    }
+}
---
+++ src/main/java/com/phuoc/learn/alg/concurrent/chap03/parallel/individual/KnnClassifierParallelIndividual.java
@@ -0,0 +1,64 @@
+package com.phuoc.learn.alg.concurrent.chap03.parallel.individual;
+
+import com.phuoc.learn.alg.concurrent.chap03.data.Distance;
+import com.phuoc.learn.alg.concurrent.chap03.data.Sample;
+
+import java.util.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class KnnClassifierParallelIndividual {
+    private final List<? extends Sample> dataSet;
+    private final int k;
+    private final ThreadPoolExecutor executor;
+    private final int numThreads;
+    private final boolean parallelSort;
+
+    public KnnClassifierParallelIndividual(List<? extends Sample> dataSet,
+                                           int k,
+                                           int factors,
+                                           boolean parallelSort) {
+        this.dataSet = dataSet;
+        this.k = k;
+        this.numThreads = factors * Runtime.getRuntime().availableProcessors();
+        this.executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(numThreads);
+        this.parallelSort = parallelSort;
+    }
+
+    public String classify(Sample example) throws InterruptedException {
+        Distance[] distances = new Distance[dataSet.size()];
+        CountDownLatch endController = new CountDownLatch(dataSet.size());
+
+        int index = 0;
+        for (Sample localExample : dataSet) {
+            IndividualDistanceTask task = new IndividualDistanceTask(distances, index,
+                                                                     localExample,example,
+                                                                     endController);
+            executor.execute(task);
+            index++;
+        }
+
+        endController.await();
+
+        if (parallelSort) {
+            Arrays.parallelSort(distances);
+        } else {
+            Arrays.sort(distances);
+        }
+
+        Map<String, Integer> results = new HashMap<>();
+        for (int i = 0; i < k; i++) {
+            Sample localExample = dataSet.get(distances[i].getIndex());
+            String tag = localExample.getTag();
+            results.merge(tag, 1, (a,b) -> a +b);
+        }
+
+        return Collections.max(results.entrySet(),
+                               Map.Entry.comparingByValue()).getKey();
+    }
+
+    public void destroy() {
+        executor.shutdown();
+    }
+}
